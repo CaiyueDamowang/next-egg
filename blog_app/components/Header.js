267b78d6a843f0b3898d07b15f3f717f9c069f37
CaiyueDamowang <www.caiyue@qq.com>
2020-06-10 14:38:12 +0800

    fix: bug

--- blog_app/components/Header.js
+++ blog_app/components/Header.js
@@ -3,10 +3,10 @@ import '@/public/styles/components/header.less'
 import  Router from "next/router";
 import { Row, Col, Menu } from "antd";
 
-const { Item } = Menu
+const { Item, SubMenu } = Menu
 export default function Head() {
     const headerMenu = [
-        { key:"home", type:"home", text: "博客", route:'blog'},
+        { key:"home", type:"home", text: "博客", route:'blog', children: [{ text: 'vuex', route: 'vuex'}]},
         { key:"video", type:"yotube", text: "视频", route: 'video'},
         { key:"life", type:"smile", text: "生活", route: 'life'},
     ]
@@ -24,6 +24,7 @@ export default function Head() {
                                 { i.text }
                             </Item>
                         )
+
                     })}
                 </Menu>
             </Row>
--- blog_app/pages/index.js
+++ blog_app/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '@/components/Header'
-import { Row, Col } from "antd";
-
 
 export default function Home() {
 
